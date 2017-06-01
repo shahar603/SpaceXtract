7e1a57cc658df0115e45d7d98969d476d7fe4d8d
shahar603 <shahar.s603@gmail.com>
2017-06-01 10:50:53 +0300

    Added a function to calculate Mach number.

--- manipulate_data.py
+++ manipulate_data.py
@@ -4,6 +4,8 @@ import simplejson as json
 from skaero.atmosphere import coesa
 from collections import OrderedDict
 import datetime
+import sys
+import websocket
 
 G = 6.67408 * 10 ** -11  # m^3 kg^-1 s^-2
 EARTH_RADIUS = 6.375 * 10 ** 6  # m
@@ -12,7 +14,7 @@ g0 = 9.80665 # m/s^2
 
 # Constants
 ACCELERATION_INTERVAL = 1  # s
-VELOCITY_INTERVAL = 5  # s
+VELOCITY_INTERVAL = 5 # s
 ANGLE_INTERVAL = 5 # s
 
 FAIRING_AREA = 25.66  # m^2
@@ -23,6 +25,7 @@ S1_dry_mass = 36000
 S1_prop_mass = 404600
 S2_dry_mass = 5000
 S2_prop_mass = 103000
+PAYLOAD_MASS = 6000
 
 
 PE_0 = - G * EARTH_MASS / EARTH_RADIUS
@@ -30,12 +33,14 @@ PE_0 = - G * EARTH_MASS / EARTH_RADIUS
 dv = 408
 
 
+#ws = websocket.create_connection("ws://192.168.0.101:13900")
+
+
 def check_angle(prev_data, data, prev_phi, phi):
-    dt = data['time'] - prev_data['time']
-    return degrees(prev_phi - phi)/dt < 5
+    return True
 
 
-def round_to_n_digits(number, n):
+def rtnd(number, n):
     return int(number * 10 ** n) / 10 ** n
 
 
@@ -44,9 +49,19 @@ def delta_mass(prev_data, data, thrust, isp):
     return dt * thrust/(isp*g0)
 
 
+def mach_number(data):
+    h, T, p, rho = coesa.table(1000 * data['altitude'])
+    return data['velocity']/sqrt(1.66*p/rho)
+
+
 def calc_thrust(Fg, Fd, current_acceleration, theta, total_mass):
     total_F = total_mass * current_acceleration
-    return total_F + Fd + Fg * sin(theta)
+
+    a = 1
+    if theta != 0:
+        a = sin(theta)/fabs(sin(theta))
+
+    return total_F + a*Fd + Fg*fabs(sin(theta))
 
 
 def calc_g(current_data):
@@ -63,7 +78,7 @@ def calc_drag(current_data):
     if type(rho) == complex:
         return 0
 
-    return 0.5 * Cd * rho * FAIRING_AREA * current_data['velocity'] ** 2
+    return 0.5 * (Cd + mach_number(current_data))* rho * FAIRING_AREA * current_data['velocity'] ** 2
 
 
 def calc_x_distance(previous_data, current_data, current_horizontal_velocity):
@@ -101,11 +116,14 @@ def calc_velocity_components(previous_data, current_data):
     if fabs(current_data['velocity']) < fabs(current_vertical_velocity):
         return None, None
 
-    return sqrt(current_data['velocity'] ** 2 - current_vertical_velocity ** 2), current_vertical_velocity
+    return sqrt(current_data['velocity']**2 - current_vertical_velocity**2), current_vertical_velocity
+
 
 
 def analyze_capture(file, dfile):
-    prev_data = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(file.readline())
+    line = file.readline()
+    
+    prev_data = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(line)#['data']
 
     prev_acceleration_data = prev_data
     prev_altitude_data = prev_data
@@ -130,9 +148,13 @@ def analyze_capture(file, dfile):
 
     uid = 0
 
-    for line in file:
-        data = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(line)
+    while True:
+        line = file.readline()#data_fron_websock()
 
+        if len(line) == 0:
+            break
+
+        data = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(line)#['data']
         if data['time'] - prev_acceleration_data['time'] >= ACCELERATION_INTERVAL:
             acceleration = calc_acceleration(prev_acceleration_data, data)
 
@@ -142,13 +164,13 @@ def analyze_capture(file, dfile):
             F = calc_thrust(Fg, Fd, acceleration, phi, mass)
 
             if not stage_sep:
-                S1_prop_mass -= delta_mass(prev_acceleration_data, data, F, 311)
-                mass = S1_dry_mass + S1_prop_mass + S2_dry_mass + S2_prop_mass
-                MAX_THRUST =  7.607*10**6
+                S1_prop_mass -= max(0, delta_mass(prev_acceleration_data, data, F, 311))
+                mass = S1_dry_mass + S1_prop_mass + S2_dry_mass + S2_prop_mass + PAYLOAD_MASS
+                MAX_THRUST = 7.607*10**6
             else:
-                S2_prop_mass -= delta_mass(prev_acceleration_data, data, F, 348)
-                mass = S2_dry_mass + S2_prop_mass
-                MAX_THRUST = 9.34*10**5
+                S2_prop_mass -= max(0, delta_mass(prev_acceleration_data, data, F, 348))
+                mass = S2_dry_mass + S2_prop_mass + PAYLOAD_MASS
+                MAX_THRUST = 8.45*10**5
 
             prev_acceleration_data = data
 
@@ -157,17 +179,17 @@ def analyze_capture(file, dfile):
 
         if data['time'] - prev_altitude_data['time'] >= VELOCITY_INTERVAL:
             Vx, Vr = calc_velocity_components(prev_altitude_data, data)
-
+            
             if Vx is not None:
                 velocity_x = Vx
                 velocity_r = Vr
 
-            if data['time'] >= 30 and check_angle(prev_phi_data, data, phi, asin(velocity_r / data['velocity'])) and data['time'] > ANGLE_INTERVAL:
+
+            if data['time'] >= 40 and data['time'] - prev_phi_data['time'] >= ANGLE_INTERVAL and data['velocity'] != 0 and \
+            fabs(velocity_r / data['velocity']) <= 1:
                 phi = asin(velocity_r / data['velocity'])
                 prev_phi_data = data
-            else:
-                prev_phi_data = data
-
+            
             downrange_distance += calc_x_distance(prev_altitude_data, data, velocity_x)
             prev_altitude_data = data
 
@@ -184,37 +206,38 @@ def analyze_capture(file, dfile):
 
 
         dict_data = OrderedDict([
-            ('time', round_to_n_digits(data['time'], 2)),
-            ('velocity', round_to_n_digits(data['velocity'], 2)),
-            ('altitude', round_to_n_digits(data['altitude'], 2)),
-            ('acceleration', round_to_n_digits(acceleration, 2)),
-            ('force', 100 * round_to_n_digits(F, 2) / MAX_THRUST),
-            ('s1_mass', round_to_n_digits(S1_prop_mass, 2)),
-            ('s2_mass', round_to_n_digits(S2_prop_mass, 2)),
-            ('vertical_velocity', round_to_n_digits(velocity_r, 2)),
-            ('horizontal_velocity', round_to_n_digits(velocity_x, 2)),
-            ('angle', round_to_n_digits(degrees(phi), 2)),
+            ('time', rtnd(data['time'], 2)),
+            ('velocity', rtnd(data['velocity'], 2)),
+            ('altitude', rtnd(data['altitude'], 2)),
+            ('acceleration', rtnd(acceleration, 2)),
+            ('force', 100 * rtnd(F, 2) / MAX_THRUST),
+            ('s1_mass', rtnd(S1_prop_mass, 2)),
+            ('s2_mass', rtnd(S2_prop_mass, 2)),
+            ('vertical_velocity', rtnd(velocity_r, 2)),
+            ('horizontal_velocity', rtnd(velocity_x, 2)),
+            ('angle', rtnd(degrees(phi), 2)),
             ('downrange_distance', downrange_distance),
-            ('gravity', round_to_n_digits(Fg, 2) / mass),
-            ('g_force', round_to_n_digits(g_force, 2)),
-            ('drag', round_to_n_digits(Fd, 2) / mass),
-            ('pe', round_to_n_digits(PE, 2)),
-            ('horizontal_ke', round_to_n_digits(KE_x, 2)),
-            ('vertical_ke', round_to_n_digits(KE_r, 2)),
-            ('ce', round_to_n_digits(centripetal_acceleration, 2)),
+            ('gravity', rtnd(Fg, 2) / mass),
+            ('g_force', rtnd(g_force, 2)),
+            ('drag', rtnd(Fd, 2) / mass),
+            ('pe', rtnd(PE, 2)),
+            ('horizontal_ke', rtnd(KE_x, 2)),
+            ('vertical_ke', rtnd(KE_r, 2)),
+            ('ce', rtnd(centripetal_acceleration, 2)),
+            ('thrust', rtnd(F, 2))
         ])
 
-        #print(json.dumps(OrderedDict(
-        #    [('uid', uid), ('timestamp', datetime.datetime.utcnow().isoformat() + 'Z'), ('data', dict_data)])))
+        print(json.dumps(OrderedDict(
+            [('uid', uid), ('timestamp', datetime.datetime.utcnow().isoformat() + 'Z'), ('data', dict_data)])))
 
-        dfile.write(json.dumps(dict_data, dfile) + '\n')
+        dfile.write(json.dumps(dict_data) + '\n')
 
         prev_data = data
         uid += 2
 
 
 def main():
-    file = open('telemetry.json', 'r')
+    file = open(sys.argv[1], 'r')
     dfile = open('a.txt', 'w')
     analyze_capture(file, dfile)
 
